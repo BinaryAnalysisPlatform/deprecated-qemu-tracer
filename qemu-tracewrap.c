04a5d7027c89eed5b4c21cc341a22720abd6637b
Manuel Egele <megele@cmu.edu>
2014-04-02 12:00:23 -0400

    tracewrap cronfigurable

---
+++ qemu-tracewrap.c
@@ -0,0 +1,130 @@
+#include "qemu/tracewrap.h"
+#include "trace_consts.h"
+#include <string.h>
+
+Frame * g_frame;
+struct toc_entry *toc;
+struct toc_entry *cur_toc_entry;
+uint64_t num_frames;
+uint64_t frames_per_toc_entry = 1LL;
+uint32_t open_frame = 0;
+
+FILE *qemu_tracefile;
+
+void do_qemu_set_trace(const char *tracefilename)
+{
+        printf("Setting qemu_tracefile at %s\n", tracefilename);
+        qemu_tracefile = fopen(tracefilename, "wb");
+        fseek(qemu_tracefile, first_frame_offset, SEEK_SET);
+        toc = (struct toc_entry *)malloc(sizeof(struct toc_entry));
+        cur_toc_entry = toc;
+}
+
+void qemu_trace_newframe(uint64_t addr, int thread_id, uint32_t insn)
+{
+    if (open_frame)
+    {
+        //qemu_log("left frame open!\n");
+        qemu_trace_endframe(); 
+    }
+    open_frame = 1;
+
+    thread_id = 1;
+    g_frame = (Frame *)malloc(sizeof(Frame));
+    frame__init(g_frame);
+
+    StdFrame *sframe = (StdFrame *)malloc(sizeof(StdFrame));
+    std_frame__init(sframe);
+    g_frame->std_frame = sframe;
+ 
+    sframe->address = addr;
+    sframe->thread_id = thread_id;
+
+    size_t len = sizeof(insn);
+    sframe->rawbytes.len = len;
+    sframe->rawbytes.data = (uint8_t *)malloc(len);
+    memcpy(sframe->rawbytes.data, &insn, len);
+
+    OperandValueList *ol_in = (OperandValueList *)malloc(sizeof(OperandValueList));
+    operand_value_list__init(ol_in);
+    ol_in->n_elem = 0;
+    sframe->operand_pre_list = ol_in;
+
+    OperandValueList *ol_out = (OperandValueList *)malloc(sizeof(OperandValueList));
+    operand_value_list__init(ol_out);
+    ol_out->n_elem = 0;
+    sframe->operand_post_list = ol_out;
+}
+
+void qemu_trace_add_operand(OperandInfo *oi, int inout)
+{
+    OperandValueList *ol;
+    if (inout & 0x1)
+    {
+            ol = g_frame->std_frame->operand_pre_list;
+    } else {
+            ol = g_frame->std_frame->operand_post_list;
+    }
+
+    ol->n_elem += 1;
+    ol->elem = realloc(ol->elem, sizeof(OperandInfo *) * ol->n_elem);
+    ol->elem[ol->n_elem - 1] = oi;
+}
+
+void qemu_trace_endframe(void)
+{
+    size_t msg_size = frame__get_packed_size(g_frame);
+    uint8_t *packed_buffer = (uint8_t *)malloc(msg_size);
+    uint64_t packed_size = frame__pack(g_frame, packed_buffer);
+    fwrite(&packed_size, 8, 1, qemu_tracefile);
+    fwrite(packed_buffer, packed_size, 1, qemu_tracefile);
+    free(packed_buffer);
+    uint64_t offset = ftell(qemu_tracefile);
+    cur_toc_entry->offset = offset;
+    cur_toc_entry->next = (struct toc_entry *)malloc(sizeof(struct toc_entry));
+    cur_toc_entry = cur_toc_entry->next;
+    cur_toc_entry->next = NULL;
+    cur_toc_entry->offset = 0;
+
+    //counting num_frames in newframe does not work by far ... 
+    //how comes? disas_arm_insn might not always return at the end?
+    num_frames++;
+    open_frame = 0;
+}
+
+void qemu_trace_finish(uint32_t exit_code)
+{
+    if (num_frames > 0)
+    {
+        uint64_t toc_offset = ftell(qemu_tracefile);
+
+        fwrite(&frames_per_toc_entry, sizeof(frames_per_toc_entry), 1, qemu_tracefile);
+
+        struct toc_entry * te = toc;
+        fwrite(&first_frame_offset, sizeof(first_frame_offset), 1, qemu_tracefile);
+        uint64_t i = 0;
+        for(i = 0; i < num_frames - 2; i++)
+        {
+            fwrite(&te->offset, sizeof(te->offset), 1, qemu_tracefile);
+            te = te->next;
+        }
+        fseek(qemu_tracefile, magic_number_offset, SEEK_SET);
+        fwrite(&magic_number, sizeof(magic_number), 1, qemu_tracefile);
+        fseek(qemu_tracefile, trace_version_offset, SEEK_SET);
+        fwrite(&out_trace_version, sizeof(out_trace_version), 1, qemu_tracefile);
+        fseek(qemu_tracefile, bfd_arch_offset, SEEK_SET);
+        fwrite(&bfd_arch, sizeof(bfd_arch), 1, qemu_tracefile);
+        fseek(qemu_tracefile, bfd_machine_offset, SEEK_SET);
+        fwrite(&bfd_machine, sizeof(bfd_machine), 1, qemu_tracefile);
+        fseek(qemu_tracefile, num_trace_frames_offset, SEEK_SET);
+        fwrite(&num_frames, sizeof(num_frames), 1, qemu_tracefile);
+        fseek(qemu_tracefile, toc_offset_offset, SEEK_SET);
+        fwrite(&toc_offset, sizeof(toc_offset), 1, qemu_tracefile);
+    }
+    fclose(qemu_tracefile);
+}
+
+void qemu_trace(Frame frame)
+{
+     return;//qemu_log("I should protobuf here\n");
+}
---
+++ target-arm/trace_helper.c
@@ -0,0 +1,157 @@
+#include <stdint.h>
+
+#include "cpu.h"
+//#include "exec/def-helper.h"
+#include "helper.h"
+#include "qemu/tracewrap.h"
+#include "qemu/log.h"
+
+uint32_t HELPER(trace_cpsr_read)(CPUARMState *env)
+{
+    uint32_t res = cpsr_read(env) & ~CPSR_EXEC;
+
+    OperandInfo * oi = load_store_reg(REG_CPSR, res, 0);
+
+    qemu_trace_add_operand(oi, 0x1);
+
+    printf("cpsr_read: 0x%x\n", res);
+
+    return res;
+}
+
+void HELPER(trace_cpsr_write)(CPUARMState *env, uint32_t val, uint32_t mask)
+{
+    printf("cpsr_write: 0x%x\n", val);
+
+    OperandInfo * oi = load_store_reg(REG_CPSR, val, 1);
+
+    qemu_trace_add_operand(oi, 0x2);
+}
+
+void HELPER(trace_newframe)(CPUARMState *env, uint32_t insn)
+{
+        qemu_trace_newframe(env->regs[15], 0, insn);
+}
+
+void HELPER(trace_endframe)(CPUARMState *env)
+{
+        qemu_trace_endframe();
+}
+
+OperandInfo * load_store_mem(uint32_t addr, uint32_t val, int ls)
+{
+        MemOperand * mo = (MemOperand *)malloc(sizeof(MemOperand));
+        mem_operand__init(mo);
+
+        mo->address = addr;
+
+        OperandInfoSpecific *ois = (OperandInfoSpecific *)malloc(sizeof(OperandInfoSpecific));
+        operand_info_specific__init(ois);
+        ois->mem_operand = mo;
+
+        OperandUsage *ou = (OperandUsage *)malloc(sizeof(OperandUsage));
+        operand_usage__init(ou);
+        if (ls == 0)
+        {
+                ou->read = 1;
+        } else {
+                ou->written = 1;
+        }
+        OperandInfo *oi = (OperandInfo *)malloc(sizeof(OperandInfo));
+        operand_info__init(oi);
+        oi->bit_length = 0;
+        oi->operand_info_specific = ois;
+        oi->operand_usage = ou;
+        oi->value.len = 4;
+        oi->value.data = malloc(oi->value.len);
+        memcpy(oi->value.data, &val, 4);
+
+        return oi;
+}
+
+OperandInfo * load_store_reg(uint32_t reg, uint32_t val, int ls)
+{
+        RegOperand * ro = (RegOperand *)malloc(sizeof(RegOperand));
+        reg_operand__init(ro);
+
+        char * reg_name = (char *)malloc(4);
+        sprintf(reg_name, "r%02d", reg);
+        ro->name = reg_name;
+
+        OperandInfoSpecific *ois = (OperandInfoSpecific *)malloc(sizeof(OperandInfoSpecific));
+        operand_info_specific__init(ois);
+        ois->reg_operand = ro;
+
+        OperandUsage *ou = (OperandUsage *)malloc(sizeof(OperandUsage));
+        operand_usage__init(ou);
+        if (ls == 0)
+        {
+                ou->read = 1;
+        } else {
+                ou->written = 1;
+        }
+        OperandInfo *oi = (OperandInfo *)malloc(sizeof(OperandInfo));
+        operand_info__init(oi);
+        oi->bit_length = 0;
+        oi->operand_info_specific = ois;
+        oi->operand_usage = ou;
+        oi->value.len = 4;
+        oi->value.data = malloc(oi->value.len);
+        memcpy(oi->value.data, &val, 4);
+
+        return oi;
+}
+
+void HELPER(log_store_cpsr)(CPUARMState *env)
+{
+        uint32_t val = cpsr_read(env);
+
+        OperandInfo * oi = load_store_reg(REG_APSR, val, 1);
+
+        qemu_trace_add_operand(oi, 0x2);
+}
+
+void HELPER(log_read_cpsr)(CPUARMState *env)
+{
+        uint32_t val = cpsr_read(env);
+
+        OperandInfo * oi = load_store_reg(REG_APSR, val, 0);
+
+        qemu_trace_add_operand(oi, 0x1);
+}
+
+void HELPER(trace_load_reg)(uint32_t reg, uint32_t val)
+{
+        qemu_log("This register (r%d) was read. Value 0x%x\n", reg, val);
+
+        OperandInfo *oi = load_store_reg(reg, val, 0);
+
+        qemu_trace_add_operand(oi, 0x1);
+}
+
+void HELPER(trace_store_reg)(uint32_t reg, uint32_t val)
+{
+        qemu_log("This register (r%d) was written. Value: 0x%x\n", reg, val);
+
+        OperandInfo *oi = load_store_reg(reg, val, 1);
+
+        qemu_trace_add_operand(oi, 0x2);
+}
+
+void HELPER(trace_ld)(CPUARMState *env, uint32_t val, uint32_t addr)
+{
+        qemu_log("This was a read 0x%x addr:0x%x value:0x%x\n", env->regs[15], addr, val);
+
+        OperandInfo *oi = load_store_mem(addr, val, 0);
+
+        qemu_trace_add_operand(oi, 0x1);
+}
+
+void HELPER(trace_st)(CPUARMState *env, uint32_t val, uint32_t addr)
+{
+        qemu_log("This was a store 0x%x addr:0x%x value:0x%x\n", env->regs[15], addr, val);
+
+        OperandInfo *oi = load_store_mem(addr, val, 1);
+
+        qemu_trace_add_operand(oi, 0x2);
+}
